d5c03ce851bb09894e00de561a70ab948b71b1c1
jason-den <hbxzknight@gmail.com>
2022-04-12 11:57:23 +1000

    parent call child function V1 - state staled

--- index.tsx
+++ index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import { render } from 'react-dom';
 import Hello from './Hello';
 import './style.css';
@@ -8,26 +8,57 @@ interface AppState {
   name: string;
 }
 
-function normalFunction() {}
-const arrowFunction = () => {};
+type TaskURL = '/big-task' | '/small-task';
+
+const Child = ({ setCallbackOnTaskCreated }) => {
+  const [taskURL, setTaskURL] = useState<TaskURL>('/big-task');
+
+  // log as simulation for history.push(redirectionRoute)
+  const onTaskCreated = () => console.log('redirectionRoute:', taskURL);
+
+  useEffect(() => {
+    setCallbackOnTaskCreated(onTaskCreated);
+  }, []);
+  return (
+    <div style={{ borderStyle: 'solid', margin: '5px', padding: '5px' }}>
+      current Task URL: {taskURL}
+      <button
+        onClick={() =>
+          setTaskURL(taskURL === '/big-task' ? '/small-task' : '/big-task')
+        }
+      >
+        toggle task type
+      </button>
+    </div>
+  );
+};
+
 class App extends Component<AppProps, AppState> {
   constructor(props) {
     super(props);
     this.state = {
       name: 'React',
+      onTaskCreated: null,
     };
   }
-  classMethod = () => {};
+  setCallbackOnTaskCreated = (callback: any) =>
+    this.setState({ onTaskCreated: callback });
+  createTask = () => {
+    const { onTaskCreated } = this.state;
+    if (onTaskCreated instanceof Function) onTaskCreated();
+  };
 
   render() {
-    console.log(typeof normalFunction);
-    console.log(typeof arrowFunction);
-    console.log(typeof this.classMethod);
-
+    const { onTaskCreated } = this.state;
     return (
-      <div>
-        <Hello name={this.state.name} />
-        <p>Start editing to see some magic happen :)</p>
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '10px' }}>
+        This is parent
+        <button onClick={this.createTask}>create task</button>
+        <div>onTaskCreated: {JSON.stringify(onTaskCreated)}</div>
+        <div>
+          This is Child.
+          <Child setCallbackOnTaskCreated={this.setCallbackOnTaskCreated} />
+        </div>
       </div>
     );
   }
